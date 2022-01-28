9207a42179779c75dc1f1cf00426078d32e02c8f
samuel Osweiler <32417612+SamuelGarm@users.noreply.github.com>
2022-01-27 19:38:55 -0700

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/stephanedorotich/CPSC449 into
    main
    
    Conflicts:
    	src/code/main.java
    
    No Ideaq what the above means but I added the basics for input file
    parsing

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,54 @@
+import java.util.ArrayList;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
+class InputNode
+{
+	char first;
+	char second;
+	int penalty;
+	
+	public InputNode(char _first, char _second)
+	{
+		first = _first;
+		second = _second;
+		penalty = -1;
+	}
+	
+	public InputNode(char _first, char _second, char _penalty)
+	{
+		first = _first;
+		second = _second;
+		penalty = _penalty;
+	}
+}
 
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	ArrayList<InputNode> partialAssignments = new ArrayList<InputNode>();
+	ArrayList<InputNode> forbiddenMachines = new ArrayList<InputNode>();
+	ArrayList<InputNode> tooNearTaks = new ArrayList<InputNode>();
+	ArrayList<InputNode> tooNearPenalties = new ArrayList<InputNode>();
+	int[][] machinePenalties = new int[8][8];
+	File myFile;
+	Scanner myReader;
+	
+	public void createScanner()
+	{	  
+		try
+		{
+			myFile = new File("filename.txt");
+			myReader = new Scanner(myFile);
+		} catch (FileNotFoundException e) {
+    	  System.out.println("An error occurred.");
+        	e.printStackTrace();
+      	}
+	}
+	
+	public static void main(String[] args) 
+	{
+		Main program = new Main();
+		program.createScanner();
 	}
 
 }
